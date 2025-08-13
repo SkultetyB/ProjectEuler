0b4b8934032271e6abe0ba6557a08dbb4ead0d89
SkultetyB <skultetybendeguz.info@gmail.com>
2025-08-13 16:46:24 +0200

    Problem 4
    Largest polindrom product

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -2,37 +2,48 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 
- 
+bool isPalindrom(int x)
+{
+	std::string str = std::to_string(x);
+	int lengthOfNumber = str.length();
+	for (int i = 0; i < lengthOfNumber-1; i++)
+	{
+		if (str[i] != str[lengthOfNumber - 1 - i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
 int main()
 {
-    std::cout << "Give me a large number:";
-    long long n; 
-    std::cin >> n;
+	std::vector<int> list;
+	for (int i = 100; i < 1000; i++)
+	{
 
-    //long long n = 600851475143;
-    long long factor = 2;
-    long long largest = 0;
-
-    while (n > 1)
-    {
-        if (n % factor == 0)
-        {
-            largest = factor;
-            n = n / factor;
-        }
-        else
-        {
-            if (factor % 2 == 0)
-            {
-                factor++;
-            }
-            else
-            {
-                factor += 2;
-            }
-        }
-    }
-    std::cout << largest;
+		for (int j = 100; j < 1000; j++)
+		{
+			
+			if (isPalindrom(i*j) ==true)
+			{
+				std::cout << i*j << i << j << "\n";
+				list.push_back(i * j);
+			}
+		}
+	}
+	int largest = 0;
+	for (int i = 0; i < list.size(); i++)
+	{
+		if (list[i] > largest)
+		{
+			largest = list[i];
+		}
+	}
+	std::cout << largest;	
 }
  
