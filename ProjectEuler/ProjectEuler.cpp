385c1fb8e9da8ab01630e21ca40e6657b856402f
SkultetyB <skultetybendeguz.info@gmail.com>
2025-08-13 15:29:13 +0200

    problem 3

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -3,28 +3,36 @@
 
 #include <iostream>
 
-class Vector
-{
-
-};
-
-
+ 
 int main()
 {
-    int sum = 0;
-    int current = 8;
-    int prev = 5;
-    int temp;
-    while (current <= 4000000)
-    {
-        if (current % 2 == 0)
-        {
-            sum += current;
-        }
-        temp = prev;
-        prev = current;
-        current += temp;
-    }
+    std::cout << "Give me a large number:";
+    long long n; 
+    std::cin >> n;
 
-    std::cout << sum + 2;
+    //long long n = 600851475143;
+    long long factor = 2;
+    long long largest = 0;
+
+    while (n > 1)
+    {
+        if (n % factor == 0)
+        {
+            largest = factor;
+            n = n / factor;
+        }
+        else
+        {
+            if (factor % 2 == 0)
+            {
+                factor++;
+            }
+            else
+            {
+                factor += 2;
+            }
+        }
+    }
+    std::cout << largest;
 }
+ 
