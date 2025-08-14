eac6be798a0dcb46a8f10cf6655f54fd6aef4d91
SkultetyB <skultetybendeguz.info@gmail.com>
2025-08-14 10:16:45 +0200

    problem 5 solved

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -5,48 +5,24 @@
 #include <string>
 #include <vector>
 
-bool isPalindrom(int x)
-{
-	std::string str = std::to_string(x);
-	int lengthOfNumber = str.length();
-	for (int i = 0; i < lengthOfNumber-1; i++)
-	{
-		if (str[i] != str[lengthOfNumber - 1 - i])
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
 
 int main()
 {
-	std::vector<int> list;
-	int current;
-	for (int i = 100; i < 1000; i++)
-	{
-		for (int j = 100; j < 1000; j++)
-		{
-			current = i * j;
-			if (current > 90000)
-			{
-				if (isPalindrom(i * j) == true)
-				{
-					std::cout << current << "\n";
-					list.push_back(current);
-				}
-			}			
-		}
-	}
-	int largest = 0;
+	std::vector<int> list{ 1,2,3,2,5,7,2,3,11,13,2,17,19};
+	long long num{1};
 	for (int i = 0; i < list.size(); i++)
 	{
-		if (list[i] > largest)
-		{
-			largest = list[i];
-		}
+		num *= list[i];	
 	}
-	std::cout << largest;	
+	if (num % 20 == 0)
+	{
+		std::cout << "good" << "\n";
+	}
+	else
+	{
+		std::cout << "bad" << "\n";
+	}
+
+	std::cout << num;
 }
  
